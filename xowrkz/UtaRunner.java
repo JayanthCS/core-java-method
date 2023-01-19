b18cceee57e045cd68f1df7a1a11b6e42038d8ba
JayanthCS <jayanthcs634xworkz@gmail.com>
2023-01-19 08:36:13 -0800

    jay

---
+++ xowrkz/UtaRunner.java
@@ -0,0 +1,109 @@
+class UtaRunner{
+
+    public static void main(String[] a){
+	
+	    System.out.println("Inside main method");
+		String item = "idli";
+		int j = Uta.foodItem(item);
+		System.out.println("Return :"+j);
+		
+		String item1 = "vade";
+		int j1 = Uta.foodItem1(item1);
+		System.out.println("Return :"+j1);
+		
+		String item2 = "dose";
+		int j2 = Uta.foodItem2(item2);
+		System.out.println("Return :"+j2);
+		
+		String item3 = "rasam";
+		int j3 = Uta.foodItem3(item3);
+		System.out.println("Return :"+j3);
+		
+		String item4 = "anna";
+		int j4 = Uta.foodItem4(item4);
+		System.out.println("Return :"+j4);
+		
+		String item5 = "saru";
+		int j5 = Uta.foodItem5(item5);
+		System.out.println("Return :"+j5);
+		
+		String item6 = "sagu";
+		int j6 = Uta.foodItem6(item6);
+		System.out.println("Return :"+j6);
+		
+		String item7 = "vade";
+		int j7 = Uta.foodItem7(item7);
+		System.out.println("Return :"+j7);
+		
+		String item8 = "rice bath";
+		int j8 = Uta.foodItem8(item8);
+		System.out.println("Return :"+j8);
+		
+		String item9 = "biriyani";
+		int j9 = Uta.foodItem1(item9);
+		System.out.println("Return :"+j9);
+		
+		String item10 = "omlet";
+		int j10 = Uta.foodItem10(item10);
+		System.out.println("Return :"+j10);
+		
+		String item11 = "egg rice";
+		int j11 = Uta.foodItem11(item11);
+		System.out.println("Return :"+j11);
+		
+		String item12= "panir";
+		int j12 = Uta.foodItem12(item12);
+		System.out.println("Return :"+j12);
+		
+		String item13 = "kulcha";
+		int j13 = Uta.foodItem13(item13);
+		System.out.println("Return :"+j13);
+		
+		String item14 = "roti";
+		int j14 = Uta.foodItem14(item14);
+		System.out.println("Return :"+j14);
+		
+		String item15 = "poori";
+		int j15 = Uta.foodItem15(item15);
+		System.out.println("Return :"+j15);
+		
+		String item16 = "chapati";
+		int j16 = Uta.foodItem16(item16);
+		System.out.println("Return :"+j16);
+		
+		String item17 = "palav";
+		int j17 = Uta.foodItem17(item17);
+		System.out.println("Return :"+j17);
+		
+		String item18 = "mosru anna";
+		int j18 = Uta.foodItem18(item18);
+		System.out.println("Return :"+j18);
+		
+		String item19 = "chitranna";
+		int j19 = Uta.foodItem19(item19);
+		System.out.println("Return :"+j19);
+		
+		String item20 = "puliyogare";
+		int j20 = Uta.foodItem20(item20);
+		System.out.println("Return :"+j20);
+		
+		String item21 = "tomato bath";
+		int j21 = Uta.foodItem21(item21);
+		System.out.println("Return :"+j21);
+		
+		String item22 = "upittu";
+		int j22 = Uta.foodItem22(item22);
+		System.out.println("Return :"+j22);
+		
+		String item23 = "kesari bath";
+		int j23 = Uta.foodItem23(item23);
+		System.out.println("Return :"+j23);
+		
+		String item24 = "reve idli";
+		int j24 = Uta.foodItem24(item24);
+		System.out.println("Return :"+j24);
+		
+	System.out.println("Outside main method");
+	}
+
+}

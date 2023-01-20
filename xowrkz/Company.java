5a8a9c7eb0811483ce931bba552834a119b19eaa
JayanthCS <jayanthcs634xworkz@gmail.com>
2023-01-20 06:52:42 -0800

    jay

---
+++ xowrkz/Company.java
@@ -0,0 +1,108 @@
+class  Company{
+	
+	static String  jay(String name){
+		
+		System.out.println("Inside jay method");
+		
+		if(name == "Air India"){
+		System.out.println("Company name is :"+name);
+        return "Campbell wilson";		
+		}
+		
+		if(name == "DLF"){
+		System.out.println("Company name is :"+name);
+        return "Ashok kumar";		
+		}
+		
+		if(name == "Dabur"){
+		System.out.println("Company name is :"+name);
+        return "Mohit";		
+		}
+		
+		if(name == "Air India"){
+		System.out.println("Company name is :"+name);
+        return "Campbell wilson";		
+		}
+		
+		if(name == "Emami"){
+		System.out.println("Company name is :"+name);
+        return "Kalyan";		
+		}
+		
+		if(name == "eBay"){
+		System.out.println("Company name is :"+name);
+        return "jamie lannone";		
+		}
+		
+		if(name == "Federal bank"){
+		System.out.println("Company name is :"+name);
+        return "Shyam";		
+		}
+		
+		if(name == "Flipkart"){
+		System.out.println("Company name is :"+name);
+        return "Kalyan krishna";		
+		}
+		
+		if(name == "GAIL"){
+		System.out.println("Company name is :"+name);
+        return "Sandeep";		
+		}
+		
+		if(name == "Godrej"){
+		System.out.println("Company name is :"+name);
+        return "Gaurav";		
+		}
+		
+		if(name == "Google"){
+		System.out.println("Company name is :"+name);
+        return "Sundar pichai";		
+		}
+		
+		if(name == "HCL"){
+		System.out.println("Company name is :"+name);
+        return "C Vijaykumar";		
+		}
+		
+		if(name == "HDFC bank"){
+		System.out.println("Company name is :"+name);
+        return "Shashidar";		
+		}
+		
+		if(name == "Hero Motocorp"){
+		System.out.println("Company name is :"+name);
+        return "Pawan";		
+		}
+		
+		if(name == "Hindustan Unilever"){
+		System.out.println("Company name is :"+name);
+        return "Sanjiv";		
+		}
+		if(name == "Hindalco Industries"){
+		System.out.println("Company name is :"+name);
+        return "Mr. Satish pai";		
+		}
+		
+		if(name == "Hindustan Motors"){
+		System.out.println("Company name is :"+name);
+        return "Prakash Sahu";		
+		}
+		
+		if(name == "ICICI Bank"){
+		System.out.println("Company name is :"+name);
+        return "Sandeep";		
+		}
+		
+		if(name == "IDBI bank"){
+		System.out.println("Company name is :"+name);
+        return "Rakesh Sharma";		
+		}
+		
+		if(name == "Vodafone Idea Ltd"){
+		System.out.println("Company name is :"+name);
+        return "Akshaya";		
+		}
+		return null;
+	}
+	
+}
---
+++ xowrkz/TransportTester.java
@@ -0,0 +1,86 @@
+class TransportTester{
+	
+	
+	public static void main(String [] a){
+		
+		System.out.println("Inside main method");
+		
+		int ref = Transport.route("Cheeranahalli");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Goa");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Chikkamagaluru");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Kaduru");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Tipturu");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Gokarna");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Tumkuru");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Arasikere");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Banavar");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Devanuru");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Biruru");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Ajjampura");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Bukkambudhi");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Penya");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Yeshwanthpura");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Rajajinagar");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Kengeri");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Dasarahalli");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Shivani");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Kuvempunagar");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("xyz");
+		System.out.println("price is :"+ref);
+		
+	    ref = Transport.route("abc");
+		System.out.println("price is :"+ref);
+		
+	    ref = Transport.route("Nagenahalli");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("Mysuru");
+		System.out.println("price is :"+ref);
+		
+		ref = Transport.route("qwe");
+		System.out.println("price is :"+ref);
+		
+		
+		System.out.println("Outside main method");
+	}
+}
